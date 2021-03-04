a89529ec6ad265d3e64b9e1c5150b6f28b303ef7
Elmehdi <eaitbrahim@gmail.com>
2021-03-04 14:41:49 -0500

    Implement the get articles from mock file

---
+++ mock-articles.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: 1,
+        title: 'My first article',
+        content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, vero in nemo perferendis sed iure mollitia, quidem alias eveniet laborum laudantium impedit voluptatem ab architecto, quia corrupti debitis culpa consequuntur.',
+        description: 'This is my first article! It\'s great! Please read it',
+        key: 'my-first-article',
+        date: new Date(),
+        imageUrl: 'http://angular.io/assets/images/logos/angular/angular.png'
+      },
+      {
+        id: 2,
+        title: 'My second article',
+        content: '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur beatae blanditiis aliquid iusto cumque recusandae exercitationem tempora maxime labore dolore, mollitia eius quibusdam amet ab magnam magni. Eligendi, sunt in?</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur beatae blanditiis aliquid iusto cumque recusandae exercitationem tempora maxime labore dolore, mollitia eius quibusdam amet ab magnam magni. Eligendi, sunt in?</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur beatae blanditiis aliquid iusto cumque recusandae exercitationem tempora maxime labore dolore, mollitia eius quibusdam amet ab magnam magni. Eligendi, sunt in?</p>',
+        description: 'Also a great article',
+        key: 'the-second-article',
+        date: new Date(),
+        imageUrl: 'http://angular.io/assets/images/logos/angular/angular_solidBlack.png'
+      }
+];
--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const app = express();
 app.listen(8000, () => {
     console.log('Server is started and listening on port 8000.');
 })
+
+app.get('/',  (req, res) => {
+    res.send('Hello node js!!');
+});
+
+require('./articles')(app);
